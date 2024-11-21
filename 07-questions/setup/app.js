98710ac217ddff1d11a79c87977f4e6300328c2e
NIK MUHAMMAD ASYRAF <nikasyraf2001@gmail.com>
2024-11-21 12:13:46 +0800

    fix: Change the method to handle javascript using selectors

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -16,8 +16,11 @@ questions.forEach(function(question) {
     const btn = question.querySelector('.question-btn');
     console.log(btn);
     btn.addEventListener('click', function() {
-        questions.forEach(function() {
-            
+        questions.forEach(function(item) {
+            console.log(item);
+            if (item !== question) {
+                item.classList.remove('show-text');
+            }
         })
         question.classList.toggle('show-text');
     })
